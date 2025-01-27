4411d8f5081b095ea9ef0a6d8d6f17fd520bcede
Наталья Ащеулова <natxen@gmail.com>
2025-01-27 16:07:17 +0700

    first-iteration

--- main.go
+++ main.go
@@ -98,20 +98,31 @@ func (s ParcelService) Delete(number int) error {
 
 func main() {
 	// настройте подключение к БД
-
-	store := // создайте объект ParcelStore функцией NewParcelStore
-	service := NewParcelService(store)
-
-	// регистрация посылки
-	client := 1
-	address := "Псков, д. Пушкина, ул. Колотушкина, д. 5"
-	p, err := service.Register(client, address)
+	db, err := sql.Open("sqlite", "tracker.db")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+	defer db.Close()
+
+	store := NewParcelStore(db) // создайте объект ParcelStore функцией NewParcelStore
+	service := NewParcelService(store)
+
+	// регистрация посылки
+	fmt.Println("***   регистрация посылки")
+	client := 1
+	address := "Псков, д. Пушкина, ул. Колотушкина, д. 5"
+	p, err := service.Register(client, address)
+	//p, err := service.Register(client, address)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	//fmt.Println("p.number = ", p.Number)
 
 	// изменение адреса
+	fmt.Println()
+	fmt.Println("***   изменение адреса")
 	newAddress := "Саратов, д. Верхние Зори, ул. Козлова, д. 25"
 	err = service.ChangeAddress(p.Number, newAddress)
 	if err != nil {
@@ -120,6 +131,8 @@ func main() {
 	}
 
 	// изменение статуса
+	fmt.Println()
+	fmt.Println("***   изменение статуса")
 	err = service.NextStatus(p.Number)
 	if err != nil {
 		fmt.Println(err)
@@ -127,6 +140,8 @@ func main() {
 	}
 
 	// вывод посылок клиента
+	fmt.Println()
+	fmt.Println("***   вывод посылок клиента")
 	err = service.PrintClientParcels(client)
 	if err != nil {
 		fmt.Println(err)
@@ -134,6 +149,7 @@ func main() {
 	}
 
 	// попытка удаления отправленной посылки
+	fmt.Println("***   попытка удаления отправленной посылки")
 	err = service.Delete(p.Number)
 	if err != nil {
 		fmt.Println(err)
@@ -142,6 +158,8 @@ func main() {
 
 	// вывод посылок клиента
 	// предыдущая посылка не должна удалиться, т.к. её статус НЕ «зарегистрирована»
+	fmt.Println()
+	fmt.Println("***   вывод посылок клиента")
 	err = service.PrintClientParcels(client)
 	if err != nil {
 		fmt.Println(err)
@@ -149,6 +167,8 @@ func main() {
 	}
 
 	// регистрация новой посылки
+	fmt.Println()
+	fmt.Println("***   регистрация новой посылки")
 	p, err = service.Register(client, address)
 	if err != nil {
 		fmt.Println(err)
@@ -156,6 +176,8 @@ func main() {
 	}
 
 	// удаление новой посылки
+	fmt.Println()
+	fmt.Println("***   удаление новой посылки")
 	err = service.Delete(p.Number)
 	if err != nil {
 		fmt.Println(err)
@@ -163,6 +185,8 @@ func main() {
 	}
 
 	// вывод посылок клиента
+	fmt.Println()
+	fmt.Println("***   вывод посылок клиента")
 	// здесь не должно быть последней посылки, т.к. она должна была успешно удалиться
 	err = service.PrintClientParcels(client)
 	if err != nil {
